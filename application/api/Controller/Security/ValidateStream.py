fa536a3b65dba9991a237daff8f811b3d6fa1e4f
zxamxoe <samsudeveloper@gmail.com>
2026-09-23 20:57:50 +0700

    setingan obs 3

--- application/api/Controller/Security/ValidateStream.py
+++ application/api/Controller/Security/ValidateStream.py
@@ -7,26 +7,29 @@ from database.models import StreamKey
 
 @method_decorator(csrf_exempt, name='dispatch')
 class ValidateStreamView(View):
-    def post(self, request, *args, **kwargs):
-        # 1. Validasi keamanan: Pastikan request hanya berasal dari localhost / internal Docker
-        client_ip = request.META.get('REMOTE_ADDR')
-        allowed_ips = ['127.0.0.1', '::1', '172.17.0.1']
-        
-        if client_ip not in allowed_ips and not client_ip.startswith('172.'):
-            return HttpResponseForbidden("Akses ditolak: Bukan dari localhost.")
+	def post(self, request, *args, **kwargs):
+		# 1. Validasi keamanan: Pastikan request hanya berasal dari localhost / internal Docker
+		client_ip = request.META.get('REMOTE_ADDR')
+		allowed_ips = ['127.0.0.1', '::1', '172.17.0.1']
+		print('masuk')
+		if client_ip not in allowed_ips and not client_ip.startswith('172.'):
+			print('ditolak')
+			return HttpResponseForbidden("Akses ditolak: Bukan dari localhost.")
 
-        # 2. Ambil data stream key yang dikirim oleh Nginx-RTMP
-        stream_key = request.POST.get('name')
-        
-        # 3. Logika validasi stream key (sesuaikan dengan database Anda)
-        stream = StreamKey.objects.filter(stream=stream_key).first()
-        if stream:
-            # Status 200 OK: Nginx mengizinkan streaming dimulai
-            return HttpResponse("OK", status=200)
-        else:
-            # Status 403 Forbidden: Nginx menolak streaming
-            return HttpResponseForbidden("Stream key salah!")
+		# 2. Ambil data stream key yang dikirim oleh Nginx-RTMP
+		stream_key = request.POST.get('name')
+		
+		# 3. Logika validasi stream key (sesuaikan dengan database Anda)
+		stream = StreamKey.objects.filter(stream=stream_key).first()
+		if stream:
+			pritn('ada stream')
+			# Status 200 OK: Nginx mengizinkan streaming dimulai
+			return HttpResponse("OK", status=200)
+		else:
+			print('key salah')
+			# Status 403 Forbidden: Nginx menolak streaming
+			return HttpResponseForbidden("Stream key salah!")
 
-    def get(self, request, *args, **kwargs):
-        # Mencegah akses via browser (GET request)
-        return HttpResponseForbidden("Metode tidak diizinkan.")
+	def get(self, request, *args, **kwargs):
+		# Mencegah akses via browser (GET request)
+		return HttpResponseForbidden("Metode tidak diizinkan.")
